9c3d7bcacf051c80acd547af819759dc9acab428
Arun <kkumararun.87@gmail.com>
2016-11-10 00:27:49 +0530

    Getting Data Through Rest Controller in Angular without Service

--- arun_app/controllers/blogController.js
+++ arun_app/controllers/blogController.js
@@ -1,4 +1,19 @@
-app.controller('BlogController', function($scope) {
+app.controller('BlogController', function($scope,$http) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
+
+		    $http.get('http://localhost:8989/arun_online_collaboration/blog/allblogs').
+        success(function(data, status, headers, config) {
+            $scope.blogs = data;
+			$scope.stat=status;
+			var x=0;
+			for(x=0;x<$scope.blogs.length;x++)
+			alert($scope.blogs[x].blogDiscription);
+
+		return $scope.blogs;
+        }).error(function(data, status, headers, config) {
+
+			alert("Unable to get the values");
+		});
+
 	});
--- arun_online_collaboration/src/main/java/com/arun/pg/controller/BlogController.java
+++ arun_online_collaboration/src/main/java/com/arun/pg/controller/BlogController.java
@@ -5,23 +5,24 @@ package com.arun.pg.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.arun.pg.dao.BlogDAO;
 import com.arun.pg.model.Blog;
 
 
-@Controller
+@RestController
 @RequestMapping(value="/blog") 
 public class BlogController {
 
 	@Autowired
 private BlogDAO blogD;
 
-
+	@CrossOrigin(origins="http://127.0.0.1:8887")
 	@RequestMapping(value="/allblogs", method=RequestMethod.GET)
 	@ResponseBody
 	public List<Blog> findAllBlog(){
